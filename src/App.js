ff87780b7a24550b91753f6659a88b4c9e8fb5c3
araujo-gui <paulaalvesaraujo@outlook.com>
2021-02-22 15:35:36 -0300

    projeto atualizado

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Form from './Componentes/Form/Form';
+import Footer from './Componentes/Footer/Footer';
+import Menu from './Componentes/Menu/Menu';
+
+function App() {
+  return (
+    <div className = "App">
+      <Menu />
+      <Form />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
